f632bac48fcb049de41f36210b15d51a84a31657
molan0426 <molanli2003@gmail.com>
2022-04-14 12:45:05 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,10 +1,10 @@
 //https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+// https://www.geeksforgeeks.org/arraylist-contains-java/
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.*;
 
 public class MarkdownParse {
 
@@ -12,26 +12,28 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-
-        while(currentIndex < markdown.length() -1) {
-
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
-
+        int i = 0;
+        while (currentIndex < markdown.length()) {
+             
+                int openBracket = markdown.indexOf("[", currentIndex);
+                int closeBracket = markdown.indexOf("]", openBracket);
+                int openParen = markdown.indexOf("(", closeBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                if(!toReturn.contains(markdown.substring(openParen + 1, closeParen))){
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+                }
+                currentIndex = closeParen + 1;
+            i ++;
+            if(i == markdown.length()-1) break;
         }
 
         return toReturn;
     }
 
-
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        System.out.println(links);
     }
 }
